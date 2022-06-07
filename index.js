25c9b7f4a0ceae6ed6a20c014ecaeeeb2888eddf
arieri <ariericyron@gmail.com>
2022-06-07 04:39:43 +0300

    phase-1/phase-1-arithmetic-lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+const num1 =31
+const num2=2
+let multiply= num1*num2
+let random=+1
+Math.floor(Math.random() * 10);
+const num3=8
+const num4=76
+let mod=num4%num3
+const max=20
+Math.max(4,8,12,16,20)
